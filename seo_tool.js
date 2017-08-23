0edfd6d7c7d8600e5315a32b0bb23179a329de4b
Jacob Turner <jacob.turner870@gmail.com>
2017-08-23 14:20:37 +0200

    Update seo_tool.js

--- seo_tool.js
+++ seo_tool.js
@@ -1,5 +1,5 @@
-<! This file belongs to the project "Jeju SEO Tool" which was released under the GPL 3.0 licence>
-<! See https://github.com/batconjurer/Jeju-SEO-Tool/blob/master/LICENSE for full license details.>
+// This file belongs to the project "Jeju SEO Tool" which was released under the GPL 3.0 licence
+// See https://github.com/batconjurer/Jeju-SEO-Tool/blob/master/LICENSE for full license details.
 
 var hltLock=false;
 //var initSty='textarea {margin: 0; border-radius: 0; color:#333333; background-color:transparent; }\
